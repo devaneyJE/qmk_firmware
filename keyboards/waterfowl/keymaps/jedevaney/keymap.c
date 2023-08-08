2768148b97ff35d4a6163224c50c7757c7d423a6
devaneyJE <jed@jedlab.dev>
2023-08-08 09:07:38 -0400

    modify combos, add vi layer

--- keyboards/waterfowl/keymaps/jedevaney/keymap.c
+++ keyboards/waterfowl/keymaps/jedevaney/keymap.c
@@ -16,7 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
-// Define and configure macros
+// Define and configure combos
 enum combos {
   ALT_L,
   ALT_R,
@@ -24,18 +24,22 @@ enum combos {
   CTRL_R,
   TC_ENTER,
   IO_ENTER,
-  QW_ESC,
-  AR_CAPS
+  YSEMI_BSPC,
+  QW_TAB,
+  AR_ESC,
+  ZX_CAPS
 };
 
-const uint16_t PROGMEM alt_l_combo[] = {KC_M, KC_N, COMBO_END};
+const uint16_t PROGMEM alt_l_combo[] = {KC_N, KC_E, COMBO_END};
 const uint16_t PROGMEM alt_r_combo[] = {KC_E, KC_I, COMBO_END};
-const uint16_t PROGMEM ctrl_l_combo[] = {KC_K, KC_H, COMBO_END};
+const uint16_t PROGMEM ctrl_l_combo[] = {KC_H, KC_COMM, COMBO_END};
 const uint16_t PROGMEM ctrl_r_combo[] = {KC_COMM, KC_DOT, COMBO_END};
 const uint16_t PROGMEM tc_enter_combo[] = {KC_BSPC, KC_SPC, COMBO_END};
 const uint16_t PROGMEM enter_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM esc_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM caps_combo[] = {KC_A, KC_R, COMBO_END};
+const uint16_t PROGMEM bspc_combo[] = {KC_Y, KC_SEMI, COMBO_END};
+const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM esc_combo[] = {KC_A, KC_R, COMBO_END};
+const uint16_t PROGMEM caps_combo[] = {KC_Z, KC_X, COMBO_END};
 
 combo_t key_combos[] = {
   [ALT_L] = COMBO(alt_l_combo, LALT(KC_LEFT)),
@@ -43,20 +47,23 @@ combo_t key_combos[] = {
   [CTRL_L] = COMBO(ctrl_l_combo, LCTL(KC_LEFT)),
   [CTRL_R] = COMBO(ctrl_r_combo, LCTL(KC_RIGHT)),
   [TC_ENTER] = COMBO(tc_enter_combo, KC_ENT),
-  [IO_ENTER] = COMBO(esc_combo, KC_ENT),
-  [QW_ESC] = COMBO(esc_combo, KC_ESC),
-  [AR_CAPS] = COMBO(esc_combo, CW_TOGG),
+  [IO_ENTER] = COMBO(enter_combo, KC_ENT),
+  [YSEMI_BSPC] = COMBO(bspc_combo, KC_BSPC),
+  [QW_TAB] = COMBO(tab_combo, KC_TAB),
+  [AR_ESC] = COMBO(esc_combo, KC_ESC),
+  [ZX_CAPS] = COMBO(caps_combo, CW_TOGG),
 };
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _COLEMAKDH, // 0 | Original | Home for encoders
-    _NAVNUM, //Original, split for vi nav
-    // _NAV // 1 | vi nav
-    _SYM, // Original, change to SYMNUM
-    // _SYMNUM // 2 | combined
+    _CDH, // 0 | Original | Home for encoders
+    //_NAVNUM, //Original, split for vi nav
+    _NAV, // 1 | vi nav
+    //_SYM, // Original, change to SYMNUM
+    _SYMNUM, // 2 | combined
     _FUNC, // 3 | Original | TapDance x2, hold to activate
-    _GAME // 4 | Original, remove?
+    _VI // QWERTY Keys for standard vi keybinds
+    //_GAME // 4 | Original, remove?
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -71,10 +78,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Z,SHFT|   X  |C,COPY|   D  |V,PSTE|  | VOL |    | Page|  |   K  |   H  |   ,  |   .  |/,SHFT|
  * `----------------------------------'  `-----'    `-----'  `----------------------------------'
  *          ,------.  ,------------------------.    ,--------------------.   ,-----. 
- *          |C+L/R |  | DEL,L3 | BS,L1,RET |RET|    |  | SPC,RET | BS,L2 |   | L/R |
+ *          |C+L/R |  | DEL,L3 |  BS,L1  |     |    |  | SPC,L2 | REP,L4 |   | L/R |
  *          `------'  `------------------------'    `--------------------'   `-----'
  */
-[_COLEMAKDH] = LAYOUT(
+[_CDH] = LAYOUT(
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
   KC_A,    MT(MOD_LALT, KC_R),    MT(MOD_LSFT, KC_S),    MT(MOD_LCTL, KC_T),    LGUI_T(KC_G),         LGUI_T(KC_M),    MT(MOD_RCTL, KC_N),    MT(MOD_RSFT, KC_E),    MT(MOD_RALT, KC_I),    KC_O,
   LSFT_T(KC_Z),    KC_X,    KC_C,    KC_D,    KC_V,         KC_K,    KC_H,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
@@ -84,21 +91,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* NAV
  *
  * ,----------------------------------.                      ,----------------------------------.
- * |  ESC | PgUp |  UP  | PgDn | TO(4)|                      |   /  |   7  |   8  |   9  |   -  |
- * |  ESC | PgUp |  UP  | PgDn | TO(4)|                      |      |  INS |      |      |      |
+ * |  TAB | PgUp |  UP  | PgDn | Home |                      | Home |PgDown| PgUp |  End |      |
  * |------+------+------+------+------|                      |------+------+------+------+------|
- * | Home | Left | Down | Right| End  |                      |   =  |   4  |   5  |   6  |   +  |
- * |  TAB | Left | Down | Right| End  |                      | Left | Down |  Up  | Right|      |
+ * |  ESC | Left | Down | Right| End  |                      |   H  |   J  |   K  |   L  |      |
  * |------+------+------+------+------|  ,-----.    ,-----.  |------+------+------+------+------|
- * |      |      |  INS | TO(2)|      |  |  2  |    |  3  |  |   0  |   1  |   2  |   3  |   *  |
- * | SHIFT|      |  INS |      |PRTSCR|  |C+d/u|    | n/N |  | Home |PgDown| PgUp |  End |      |
+ * | SHIFT|      |  INS |      |PRTSCR|  |C+d/u|    | n/N |  | Left | Down |  Up  | Right|      |
  * `----------------------------------'  `-----'    `-----'  `----------------------------------'
  *          ,-----.   ,--------------------.            ,--------------------.   ,-------. 
  *          |  1  |   | DEL | SPACE | MO(3)|            | TO(0) | BS | ENTER |   |Alt+L/R|
  *          `-----'   `--------------------'            `--------------------'   `-------'
  */
-//[_NAV] = LAYOUT(
-[_NAVNUM] = LAYOUT( // remove for line above
+[_NAV] = LAYOUT(
+//[_NAVNUM] = LAYOUT( // removed for line above
   KC_ESC,    KC_PGUP,    KC_UP,    KC_PGDN,   TO(4),    KC_SLSH,    KC_7,    KC_8,    KC_9,    KC_MINS,
   KC_HOME,   KC_LEFT,    KC_DOWN,  KC_RGHT,   KC_END,  KC_EQL,     KC_4,    KC_5,    KC_6,    KC_PLUS,
   KC_NO,    KC_NO,      KC_INS,   TO(2),     KC_NO,    KC_0,       KC_1,    KC_2,    KC_3,    KC_ASTR,
@@ -112,14 +116,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|                      |------+------+------+------+------|
  * |   !  |   @  |   #  |   $  |   %  |                      |   ^  |   &  |   *  |   (  |   )  |
  * |------+------+------+------+------|  ,-----.    ,-----.  |------+------+------+------+------|
- * |   [  |   ]  |   {  |   }  |  ~`  |  |     |    |     |  |   _  |   -  |   '  |   "  |  \|  |
+ * |   [  |   ]  |   {  |   }  |  ~`  |  |     |    |     |  |   _  |   -  |  '"  |  +=  |  \|  |
  * `----------------------------------'  `-----'    `-----'  `----------------------------------'
  *          ,-----.   ,--------------------.            ,--------------------.   ,-----. 
  *          |     |   |     |       |      |            |       |    |       |   |     |
  *          `-----'   `--------------------'            `--------------------'   `-----'
  */
-//[_SYMNUM] = LAYOUT(
-[_SYM] = LAYOUT( // remove line for above
+[_SYMNUM] = LAYOUT(
+//[_SYM] = LAYOUT( // removed line for above
   KC_PERC, S(KC_QUOT),   KC_LBRC, KC_RBRC,  KC_NUBS,     RALT(KC_GRV), S(KC_GRV), KC_CIRC, KC_NO,   KC_NO,
   KC_NONUS_HASH, KC_EXLM, KC_LPRN, KC_RPRN, S(KC_NUBS),      KC_UNDS, KC_QUOT, S(KC_2), S(KC_NUHS), KC_GRV,
   KC_DLR, S(KC_3),  KC_LCBR, KC_RCBR, KC_AMPR,      TO(1), KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -144,5 +148,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_F4,   KC_F5,   KC_F6,   KC_F12,
   QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F13,
   KC_1, LT(3,KC_DEL), LT(1,KC_SPC), KC_TAB, KC_2,         KC_3, TO(0), KC_BSPC, LT(2,KC_ENT), KC_4
+),
+
+/* QWERTY
+ *
+ * ,----------------------------------.                      ,----------------------------------.
+ * |   Q  |   W  |   E  |   R  |   T  |                      |   J  |   L  |   U  |   Y  |   ;  |
+ * |------+------+------+------+------|                      |------+------+------+------+------|
+ * |   A  |R,LALT|S,SHFT|T,CTRL| G,OS |                      | M,OS |N,CTRL|E,SHFT|I,RALT|   O  |
+ * |------+------+------+------+------|  ,-----.    ,-----.  |------+------+------+------+------|
+ * |Z,SHFT|   X  |C,COPY|   D  |V,PSTE|  | VOL |    | Page|  |   K  |   H  |   ,  |   .  |/,SHFT|
+ * `----------------------------------'  `-----'    `-----'  `----------------------------------'
+ *          ,------.  ,------------------------.    ,--------------------.   ,-----. 
+ *          |C+L/R |  | DEL,L3 |  BS,L1  |     |    |  | SPC,L2 | REP,L4 |   | L/R |
+ *          `------'  `------------------------'    `--------------------'   `-----'
+ */
+[_VI] = LAYOUT(
+  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+  KC_A,    MT(MOD_LALT, KC_R),    MT(MOD_LSFT, KC_S),    MT(MOD_LCTL, KC_T),    LGUI_T(KC_G),         LGUI_T(KC_M),    MT(MOD_RCTL, KC_N),    MT(MOD_RSFT, KC_E),    MT(MOD_RALT, KC_I),    KC_O,
+  LSFT_T(KC_Z),    KC_X,    KC_C,    KC_D,    KC_V,         KC_K,    KC_H,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+  KC_1, LT(3,KC_DEL), LT(1,KC_BSPC), KC_ENT, KC_2,         KC_3, KC_NO, KC_SPC, LT(2,KC_ENT), KC_4
 ),
 };
